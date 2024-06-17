df4cbe02f572e7ce6b893fc8b848abb3ddce922d
adrianoaraujo0 <adriano.amedeiros@hotmail.com>
2024-06-17 09:09:53 -0300

    update activities

---
+++ curso-vue/src/diretiva_v_html.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+    let texto = '<p> Testando o v-html</p>';
+</script>
+
+<template>
+
+    <p>TESTE</p>
+
+    <p> {{ texto }}</p>
+
+    <div v-html="texto"></div>
+</template>
